be27b63b192980acb9feaf75f51a2f62fb6d1845
unknown <elhamhashemi56@yahoo.com>
2021-08-24 17:13:10 +0200

    add moment to manage time and fix cors bug by proxy

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,11 +3,21 @@ import DatePicker from "react-datepicker";
 import './Home.scss'
 import "react-datepicker/dist/react-datepicker.css";
 import BTCChart from "./components/BTCChart";
-
+import BitcoinService from "../../service/BitcoinService";
+import moment from "moment";
 
 const Home = () => {
-    const [startDate, setStartDate] = useState(new Date()); // today
-    const [endDate, setEndDate] = useState(new Date() - 10 * 24 * 3600); // today - 10 days ago
+
+
+    const [startDate, setStartDate] = useState(new Date(moment().add(-10, 'days'))); // today - 10 days ago
+    const [endDate, setEndDate] = useState(new Date()); // today
+
+    console.log(endDate)
+
+    const handleRender = () => {
+        console.log(startDate, endDate)
+        // BitcoinService.getDateByRange(startDate, endDate)
+    }
 
     return (
         <div className="home">
@@ -19,7 +29,7 @@ const Home = () => {
                 EndDate:
                 <DatePicker selected={endDate} onChange={(date) => setEndDate(date)}/>
             </label>
-            <button >Render</button>
+            <button onClick={handleRender}>Render</button>
             <div className="home__chart">
                 <BTCChart/>
             </div>
--- src/service/BitcoinService.js
+++ src/service/BitcoinService.js
@@ -3,6 +3,9 @@ import axios from "axios";
 class BitcoinService {
 
     static getDateByRange(start, end) {
-        return axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
+        return axios.get(`/v1/bpi/historical/close.json?start=${start}&end=${end}`)// Add proxy to package.json
+        // return axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
     }
 }
+
+export default BitcoinService
