8b4883580a7670086ebb5d7bb3fad8bad3f2618c
unknown <elhamhashemi56@yahoo.com>
2021-08-28 22:23:40 +0200

    proxy deleted

--- src/service/BitcoinService.js
+++ src/service/BitcoinService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class BitcoinService {
 
     static getDateByRange(start, end) {
-        return axios.get(`/v1/bpi/historical/close.json?start=${start}&end=${end}`)// Add proxy to package.json
+        return axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)// Add proxy to package.json
         // return axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
     }
 }
